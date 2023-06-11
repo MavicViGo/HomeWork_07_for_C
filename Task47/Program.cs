cb8403b267d958d4d598d5128c71e2675ebd76ac
Goncharov <GVVic@mail.ru>
2023-06-11 15:22:48 +0700

    Add Task47

---
+++ Task47/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+void FillArray(double[,]matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = Math.Round(new Random().NextDouble() * (-100 - 100) + 100, 2);
+        }
+    }
+}
+
+
+void PrintArray(double[,]matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размер массива ч/з пробел: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+//Console.WriteLine(size[0] + size[1]);
+double[,] array = new double[size[0], size[1]];
+FillArray(array);
+PrintArray(array);
